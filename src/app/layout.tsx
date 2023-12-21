96ca652ed6848d410f0ac1d505fad73038a2c58e
Vertori <115083330+Vertori@users.noreply.github.com>
2023-12-22 00:01:21 +0100

    footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
         <Navbar />
-        <main className="p-4 max-w-7xl m-auto min-w-[300px]">{children}</main>
+        <main className="flex-1 p-4 max-w-7xl m-auto min-w-[300px]">
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
   );
